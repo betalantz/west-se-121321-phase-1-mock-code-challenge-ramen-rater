c3b188fae403bfd8d88ca1eb16a8b8e7501524b2
betalantz <betalantz@gmail.com>
2021-12-22 15:34:54 -0800

    adds first deliverable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-// write your code here
+const baseUrl = "http://localhost:3000"
+
+// DOM selectors
+const menu = document.querySelector('#ramen-menu')
+
+
+// Listeners
+
+
+// Fetchers
+function getAllRamens(){
+    fetch(baseUrl + `/ramens`)
+        .then(res => res.json())
+        // .then((arrRamenObj) => renderAllRamens(arrRamenObj))
+        .then(renderAllRamens)
+}
+
+// Render functions
+function renderAllRamens(ramensArr){
+    ramensArr.forEach(renderOneMenu)
+}
+
+function renderOneMenu(ramenObj){
+    console.log(ramenObj)
+    const img = document.createElement("img")
+
+    img.src = ramenObj.image
+    menu.appendChild(img)
+}
+
+// Event handlers
+
+
+// Initializers
+getAllRamens()

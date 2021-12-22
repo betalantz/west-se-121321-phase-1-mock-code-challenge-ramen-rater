727dc0051ea7d67efda3ee17514a25ae26466b1e
betalantz <betalantz@gmail.com>
2021-12-22 15:49:43 -0800

    adds 2nd deliverable

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const baseUrl = "http://localhost:3000"
 
 // DOM selectors
 const menu = document.querySelector('#ramen-menu')
-
-
+const detail = document.querySelector('#ramen-detail')
+const rating = document.querySelector('#rating-display')
+const comment = document.querySelector('#comment-display')
 // Listeners
 
 
@@ -21,11 +22,21 @@ function renderAllRamens(ramensArr){
 }
 
 function renderOneMenu(ramenObj){
-    console.log(ramenObj)
     const img = document.createElement("img")
 
     img.src = ramenObj.image
+    img.addEventListener('click', () => renderDetail(ramenObj))
     menu.appendChild(img)
+}
+
+function renderDetail(ramenObj){
+    detail.innerHTML = `
+        <img class="detail-image" src="${ramenObj.image}" alt="${ramenObj.name}" />
+        <h2 class="name">${ramenObj.name}</h2>
+        <h3 class="restaurant">${ramenObj.restaurant}</h3>
+    `
+    rating.innerText = ramenObj.rating
+    comment.innerText = ramenObj.comment
 }
 
 // Event handlers
